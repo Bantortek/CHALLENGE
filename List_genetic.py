97bf834c1c9be5915f88ab7590b32c7b0069099d
supergatien <lecomodateur2.0@gmail.com>
2025-01-28 15:39:25 +0100

    Update List_genetic.py
    Manque le score et la liste pour les maps

--- List_genetic.py
+++ List_genetic.py
@@ -26,21 +26,21 @@ def Crossover(dico,nb_par_gen,nb_tri):
 # entrée : dictionnaire des dérangement trié
 # sortie : dictionnaire de dérangement de la prochaine génération (sans mutation)
     list_parent=[]
-    for key,values in dico:
+    dico_gen_suivante={}
+    for key in dico:
         if len(list)<nb_tri:
             list_parent.append(list(key))
     couples=Couple_random(nb_par_gen,nb_tri)
     for couple in couples:
         enfant1,enfant2=XXX(list_parent,couple)
-
-    
-    
-    return
+        dico_gen_suivante[tuple(enfant1)]=0
+        dico_gen_suivante[tuple(enfant2)]=0
+    return dico_gen_suivante
 
 
 def Couple_random(nb_par_gen, nb_tri):
 # entrée : nombre d'apartition d'un nombre possible
-# sortie : liste de nb_gen//2 couple de nombre sans doublon (ex (1,2) peut être dedans mais pas (2,1))
+# sortie : liste de nb_gen//2 couple de nombre (de 1 à nb_tri compris) sans doublon (ex (1,2) peut être dedans mais pas (2,1))
     list_couple=[]
     dico_apparition={}
     nb_apparition=nb_par_gen//nb_tri
@@ -66,13 +66,92 @@ def Couple_random(nb_par_gen, nb_tri):
 
 def XXX(list_parent,couple):
 # entrée : liste des parent (parent qui est une liste de dérangement) / couple d'entier des numéros des parents à associer
-# sortie : 2 liste enfants  
+# sortie : 2 liste enfants 
+    enfant1=[]
+    enfant2=[]
+    parent1=list_parent[couple[0]-1]
+    parent2=list_parent[couple[1]-1]
+    for i in range (len(parent1)//2):
+        enfant1.append(parent1[i])
+        enfant2.append(parent2[i])
+    for i in range (len(parent1)//2):
+        nb_found=True
+        j=-1
+        while nb_found:
+            j+=1
+            if parent2[-1-j] not in enfant1:
+                nb_found=False
+        if  i==0:
+            enfant1.append(parent2[-1-j])
+        else:     
+            enfant1.insert(-i,parent2[-1-j])
+        nb_found=True
+        j=-1
+        while nb_found:
+            j+=1
+            if parent1[-1-j] not in enfant2:
+                nb_found=False
+        if i==0:
+            enfant2.append(parent1[-1-j])
+        else:
+            enfant2.insert(-i,parent1[-1-j])
+    return enfant1, enfant2
+
+
+
+
+def Mutation(dico_gen_suiv,tx_mutation,nb_de_mutation):
+# entreé : dico de la génération suivante / taux de muté dans la population en % / nb d'échange dans un dérangement donné
+# Sortie : dico muté
+    list_gen=[]
+    for key in dico_gen_suiv:
+        list_gen.append(list(key))
+    nb_a_mute=int(tx_mutation*len(list_gen)/100)
+    list_a_mute=[]
+    list_element_mute=[]
+    while len(list_a_mute)<nb_a_mute:
+        nb=random.randint(0,len(list_gen)-1)
+        if nb not in list_a_mute:
+            list_a_mute.append(nb)
+        
+
+    while len(list_element_mute)<nb_a_mute:
+        list_couple=[]
+        while len(list_couple)<2*nb_de_mutation:
+            nb=random.randint(0,19)
+            if nb not in list_couple:
+                list_couple.append(nb)
+        list_element_mute.append(list_couple)
+
+    for i in range (len(list_a_mute)):
+        a_mute=list_gen[list_a_mute[i]].copy()
+        k=1
+        for j in range (len(list_element_mute[i])):
+            if k==1:
+                nb1=a_mute[list_element_mute[i][j]]
+                nb2=a_mute[list_element_mute[i][j+1]]
+                a_mute.pop(list_element_mute[i][j])
+                a_mute.insert(list_element_mute[i][j],nb2)
+
+                a_mute.pop(list_element_mute[i][j+1])
+                a_mute.insert(list_element_mute[i][j+1],nb1)
+            k*=-1
+        list_gen[list_a_mute[i]]=a_mute
     
+    dico_mute={}
+    for i in list_gen:
+        dico_mute[tuple(i)]=0
+
+    return dico_mute
 
 
-def Solution(list_pt_map,nb_gen_max=10,nb_par_gen=1000,nb_tri=10):
+
+
+def Solution(list_pt_map,nb_gen_max=10,nb_par_gen=1000,nb_tri=100,tx_mutation=75,nb_de_mutation=1):
 # entrée : matrice 20X3 qui représente la map + parametre de la selection
 # sortie : liste de len 20 qui est la meilleur solution après selection naturelle
+    if (nb_tri*nb_tri-1)<nb_par_gen:
+        return "Erreur nb_tri est trop petit"
     if nb_par_gen%nb_tri!=0:
         return "Erreur le nb_par_gen doit être multiple de nb_tri"
     if nb_tri%2!=0:
@@ -80,6 +159,14 @@ def Solution(list_pt_map,nb_gen_max=10,nb_par_gen=1000,nb_tri=10):
     dico_derangement=Initialisation_gen(nb_par_gen)
     for i in range (nb_gen_max):
         ## calcule score
-        dico_derangement=Tri_dico(dico_derangement)
-        dico_derangement=Crossover(dico_derangement,nb_par_gen,nb_tri)
-    return
+        dico_trier=Tri_dico(dico_derangement)
+        dico_gen_suiv=Crossover(dico_derangement,nb_par_gen,nb_tri)
+        dico_gen_mute=Mutation(dico_gen_suiv,tx_mutation,nb_de_mutation)
+        dico_derangement=dico_gen_mute
+    # calcule du score
+    i=0
+    for key in dico_trier:
+        if i ==0:
+            solution = list(key)
+    return solution
+
