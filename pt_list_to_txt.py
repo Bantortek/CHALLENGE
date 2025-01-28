5e7a45e8f37de9dab557ecadac1267b29b2097e6
Bantortek <148080660+Bantortek@users.noreply.github.com>
2025-01-28 14:56:05 +0100

    pt_list_to_txt v1
    première version de pt_list_to_txt.
    on y rentre une liste d'entier symbolisant l'ordre des cylindres à passer et retourne le .txt correspondant pour le robot.
    Cette première version n'esquive pas les autres cylindres

--- pt_list_to_txt.py
+++ pt_list_to_txt.py
@@ -1,24 +1,6 @@
 import math as m
-
-
-def pt_list_to_txt(pt_list, cyl_coord):
-    """
-    transforme une liste de points en liste de commandes pour le robot
-    :param pt_list: liste contenant les cylindres dans l'ordre dans lequel on veut les parcourir
-    :param cyl_coord: liste des cylindres avec leurs coordonnées en x, en y, et leur valeur en points (dimensions: n*3)
-    :return: rien (écrit directement dans un fichier texte)
-    """
-    sortie = ""
-    pos_actuelle = [0, 0, 0]  # [x,y,angle]
-    for pt in pt_list:
-        distance = m.sqrt((cyl_coord[pt][0] - pos_actuelle[0]) ** 2 + (cyl_coord[pt][1] - pos_actuelle[1]) ** 2)
-        angle_absolu = m.acos((cyl_coord[pt][0] - pos_actuelle[0]) / distance)
-        angle = round((angle_absolu - pos_actuelle[2])*180/m.pi)
-        sortie += f"TURN {angle}\nGO {distance}\n"
-    sortie += "FINISH"
-    with open("script.txt", 'w') as file:
-        file.write(sortie)
-
+import time
+import matplotlib.pyplot as plt
 
 def input_txt_to_list(txt_file):
     with open(txt_file) as file:
@@ -30,9 +12,69 @@ def input_txt_to_list(txt_file):
             sortie.append([float(x) for x in elt.split(" ") if x])
     return sortie
 
+def pt_list_to_txt(pt_list, cyl_coord):
+    """
+    transforme une liste de points en liste de commandes pour le robot
+    :param pt_list: liste contenant les cylindres dans l'ordre dans lequel on veut les parcourir
+    :param cyl_coord: liste des cylindres avec leurs coordonnées en x, en y, et leur valeur en points (dimensions: n*3)
+    :return: rien (écrit directement dans un fichier texte)
+    """
+    sortie = ""
+    pos_actuelle = [0, 0, 0]  # [x,y,angle_deg]
+    dir_act = [1, 0]
+    trajet = [[pos_actuelle[0]],
+              [pos_actuelle[1]],
+              [-m.pi / 2]
+              ]
+    for pt in pt_list:
+        distance = m.sqrt((cyl_coord[pt][0] - pos_actuelle[0]) ** 2 + (cyl_coord[pt][1] - pos_actuelle[1]) ** 2)
+        dir_obj = [cyl_coord[pt][0] - pos_actuelle[0], cyl_coord[pt][1] - pos_actuelle[1]]
 
-pt_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        angle_rad = m.atan((dir_act[0] * dir_obj[1] - dir_act[1] * dir_obj[0]) /
+                              (dir_act[0] * dir_obj[0] + dir_act[1] * dir_obj[1]))
+        if dir_act[0] * dir_obj[0] + dir_act[1] * dir_obj[1] < 0:
+            if angle_rad < 0:
+                angle_rad += m.pi
+            else:
+                angle_rad -= m.pi
+        dir_act = dir_obj
+        angle_deg = m.degrees(angle_rad)
+
+        pos_actuelle[0] = cyl_coord[pt][0]
+        pos_actuelle[1] = cyl_coord[pt][1]
+        pos_actuelle[2] = angle_deg
+
+        sortie += f"TURN {pos_actuelle[2]}\nGO {distance}\n"
+
+        # partie affichage dans le graph (affiche ce qu'on veut faire, pas ce qui est fait réellement)
+        trajet[2].append(angle_rad)
+        trajet[0].append(trajet[0][-1] + dir_act[0])
+        trajet[1].append(trajet[1][-1] + dir_act[1])
+        # ===============================
+
+    sortie += "FINISH"
+    with open("script.txt", 'w') as file:
+        file.write(sortie)
+    return trajet
+
+
+# sortir une liste [points, temps_mis, carburant_utilisé]
+
+
+pt_list = [4, 19]
 
 cyl_coord = input_txt_to_list("donnees-map.txt")
+x_list = []
+y_list = []
+for elt in cyl_coord:
+    x_list.append(elt[0])
+    y_list.append(elt[1])
+trajet = pt_list_to_txt(pt_list, cyl_coord)
 
-pt_list_to_txt(pt_list, cyl_coord)
+plt.scatter(x_list, y_list)
+for i in range(len(x_list)):
+    crcl = plt.Circle((x_list[i], y_list[i]), 0.65) # cylindres d'1m de diamètre et robot de 0.3m de diamètre
+    plt.gca().add_patch(crcl)
+plt.plot(trajet[0], trajet[1])
+plt.axis('equal')
+plt.show()
